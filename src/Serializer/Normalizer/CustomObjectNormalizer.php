c0ecd02894aa0d08c9e41a222ea5a07fd697a1ab
Piotr Rugala <piotr@isedo.pl>
2023-04-11 14:12:56 +0200

    Object normalizer fix

--- src/Serializer/Normalizer/CustomObjectNormalizer.php
+++ src/Serializer/Normalizer/CustomObjectNormalizer.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Prugala\RequestDto\Serializer\Normalizer;
 
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\PropertyInfo\PropertyTypeExtractorInterface;
+use Symfony\Component\Serializer\Mapping\ClassDiscriminatorResolverInterface;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryInterface;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class CustomObjectNormalizer extends ObjectNormalizer
 {
+    public function __construct(ClassMetadataFactoryInterface $classMetadataFactory = null, NameConverterInterface $nameConverter = null, PropertyAccessorInterface $propertyAccessor = null, PropertyTypeExtractorInterface $propertyTypeExtractor = null, ClassDiscriminatorResolverInterface $classDiscriminatorResolver = null, callable $objectClassResolver = null, array $defaultContext = [])
+    {
+        parent::__construct($classMetadataFactory, $nameConverter ?? new CamelCaseToSnakeCaseNameConverter(), $propertyAccessor, $propertyTypeExtractor, $classDiscriminatorResolver, $objectClassResolver, $defaultContext);
+    }
+
     protected function setAttributeValue(object $object, string $attribute, mixed $value, string $format = null, array $context = []): void
     {
         if ($value) {
